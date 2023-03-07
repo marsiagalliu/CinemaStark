d80be46c81d85882d9a9ac5a81403be2e2cb75e3
Marsi <agalliumars@gmail.com>
2023-03-07 15:54:02 +0100

    Initial commit

---
+++ src/main/java/com/dojo/cinemastark/CinemaStarkApplication.java
@@ -0,0 +1,13 @@
+package com.dojo.cinemastark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaStarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaStarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dojo/cinemastark/CinemaStarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.cinemastark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaStarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
